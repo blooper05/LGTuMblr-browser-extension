7e206b0894265d8096ef13909a88741f7b06dade
blooper05 <legend.of.blooper@gmail.com>
2016-03-11 10:59:37 +0900

    initial implementation

---
+++ js/popup.js
@@ -0,0 +1,6 @@
+$.getJSON('http://tumblrapi-blooper.rhcloud.com/photos', null, function (data, status) {
+  $.each(data, function () {
+    var img = $('<img>').addClass('image').attr('src', this);
+    $('.row').append(img).hide().fadeIn('fast');
+  });
+});
