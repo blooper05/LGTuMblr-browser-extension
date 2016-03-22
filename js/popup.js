9f4cfbeb1fedc556543c975656cd1d6d335627e4
blooper05 <legend.of.blooper@gmail.com>
2016-03-22 23:38:47 +0900

    add reload function

--- js/popup.js
+++ js/popup.js
@@ -14,3 +14,27 @@ $.getJSON('http://tumblrapi-blooper.rhcloud.com/photos', null, function (data, s
     });
   });
 });
+
+$('#reload').on('click', function (data) {
+  $('.image').fadeOut(function () {
+    $('#loading').fadeIn('fast');
+    this.remove();
+  });
+
+  $.getJSON('http://tumblrapi-blooper.rhcloud.com/photos', null, function (data, status) {
+    $.each(data, function () {
+      var img = $('<img>').addClass('image').attr('src', this);
+      $('.row').append(img).hide().fadeIn('fast');
+    });
+
+    $('#loading').fadeOut();
+
+    $('.image').on('click', function (data) {
+      var url = this.src;
+
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
+        chrome.tabs.sendMessage(tab[0].id, { url: url }, function (response) {});
+      });
+    });
+  });
+});
