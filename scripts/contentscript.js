95601d5fcb6cc69c389aa4c4ec038c574c71f703
blooper05 <legend.of.blooper@gmail.com>
2018-05-19 11:31:55 +0900

    :+1: Add approve function

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -2,9 +2,14 @@
 
 chrome.runtime.onMessage.addListener(request => {
   comment(request.url);
+  approve();
 });
 
 const comment = url => {
   const comment = `![LGTM](${url})`;
   document.getElementById('pull_request_review_body').value = comment;
+};
+
+const approve = () => {
+  document.getElementsByName('pull_request_review[event]')[1].checked = true;
 };
