ed30bbe77e004902cb4f41d31935b08289734b89
blooper05 <legend.of.blooper@gmail.com>
2018-05-19 11:28:56 +0900

    :+1: Add comment function

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -1,6 +1,10 @@
 'use strict';
 
 chrome.runtime.onMessage.addListener(request => {
-  const comment = `![LGTM](${request.url})`;
-  document.getElementById('pull_request_review_body').value = comment;
+  comment(request.url);
 });
+
+const comment = url => {
+  const comment = `![LGTM](${url})`;
+  document.getElementById('pull_request_review_body').value = comment;
+};
