c8f6416f9d27917d63244723342b11285d708235
blooper05 <legend.of.blooper@gmail.com>
2021-05-26 19:42:04 +0900

    🔧 (chore): Initial setup of `windicss-webpack-plugin`

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
+const { default: WindiCSSWebpackPlugin } = require('windicss-webpack-plugin');
+
 module.exports = {
+  webpack: (config) => {
+    config.plugins.push(new WindiCSSWebpackPlugin());
+    return config;
+  },
+
   experimental: {
     eslint: true,
   },
