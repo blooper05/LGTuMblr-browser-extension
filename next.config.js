e82f5089c77d7ee61133f750cc5fd5dfbce7f7c6
blooper05 <legend.of.blooper@gmail.com>
2021-05-26 19:42:49 +0900

    🔧 (chore): Use webpack5 explicitly

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const { default: WindiCSSWebpackPlugin } = require('windicss-webpack-plugin');
 
 module.exports = {
+  future: {
+    webpack5: true,
+  },
+
   webpack: (config) => {
     config.plugins.push(new WindiCSSWebpackPlugin());
     return config;
