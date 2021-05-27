644bd5538193a84a16475ba9dc070e368deb1051
blooper05 <legend.of.blooper@gmail.com>
2021-05-27 19:30:33 +0900

    Revert "🔧 (chore): Use webpack5 explicitly"
    This reverts commit e82f5089c77d7ee61133f750cc5fd5dfbce7f7c6.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 const { default: WindiCSSWebpackPlugin } = require('windicss-webpack-plugin');
 
 module.exports = {
-  future: {
-    webpack5: true,
-  },
-
   webpack: (config) => {
     config.plugins.push(new WindiCSSWebpackPlugin());
     return config;
