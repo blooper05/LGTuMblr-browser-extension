cc26912cb150df8a41445e042b0b3e39b1d2c6fb
blooper05 <legend.of.blooper@gmail.com>
2021-05-28 00:50:16 +0900

    ♻️ (refactor): Try to adopt the `classnames` style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,32 @@
+import { classnames } from 'tailwindcss-classnames';
+
 export default function Index() {
+  const classNames = {
+    main: classnames('p-8', 'w-full', 'bg-white'),
+    grid: classnames('grid', 'gap-8', 'grid-cols-4'),
+    card: classnames(
+      'm-auto',
+      'rounded-lg',
+      'shadow-lg',
+      'cursor-pointer',
+      'overflow-hidden',
+    ),
+    image: classnames('w-full', 'h-full', 'object-cover'),
+  };
+
+  const src = 'https://picsum.photos/500/750';
+
   return (
-    <div className="p-8 w-full bg-white">
-      <div className="grid gap-8 grid-cols-4">
+    <main className={classNames.main}>
+      <div className={classNames.grid}>
         {[...Array(10)].map((_, i) => {
           return (
-            <div
-              key={i}
-              className="m-auto rounded-lg shadow-lg cursor-pointer overflow-hidden"
-            >
-              <img
-                src={`https://picsum.photos/seed/${i}/500/750`}
-                className="w-full h-full object-cover"
-              />
+            <div className={classNames.card} key={i}>
+              <img className={classNames.image} src={src} />
             </div>
           );
         })}
       </div>
-    </div>
+    </main>
   );
 }
