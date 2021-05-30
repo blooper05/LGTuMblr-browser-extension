696dfede3e597c8de6922df8c4f045b6fd8502e5
blooper05 <legend.of.blooper@gmail.com>
2021-05-30 13:50:25 +0900

    ✨ (feat): Initial implementation of infinite scroll
    ⚠️ This contains a bug that causes the API to be called twice.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { classnames } from 'tailwindcss-classnames';
+import InfiniteScroll from 'react-infinite-scroller';
 import Image from '../components/Image';
 import useFetchImages from '../hooks/useRequest';
 
@@ -13,14 +14,22 @@ export default function Index() {
   if (error) return <div>Oops, something went wrong!</div>;
   if (!data) return <div>Loading...</div>;
 
+  const isLoadingMore = 0 < size && data && !data[size - 1];
+  const loadMore = () => {
+    if (!isLoadingMore) return setSize(size + 1);
+  };
+
   return (
     <main className={classNames.main}>
-      <div className={classNames.grid}>
-        {data.map((images: string[]) => {
-          return images.map((url: string) => <Image url={url} key={url} />);
-        })}
-        <button onClick={() => setSize(size + 1)}>LOAD MORE</button>
-      </div>
+      <InfiniteScroll
+        className={classNames.grid}
+        loadMore={loadMore}
+        hasMore={true}
+      >
+        {[].concat(...data).map((url: string, i: number) => (
+          <Image url={url} key={i} />
+        ))}
+      </InfiniteScroll>
     </main>
   );
 }
