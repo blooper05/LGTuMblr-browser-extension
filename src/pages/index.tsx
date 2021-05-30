ad7b9043bb09b73f3189ed9717985d1612079cb1
blooper05 <legend.of.blooper@gmail.com>
2021-05-30 13:50:36 +0900

    Revert "✨ (feat): Initial implementation of infinite scroll"
    This reverts commit 696dfede3e597c8de6922df8c4f045b6fd8502e5.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import { classnames } from 'tailwindcss-classnames';
-import InfiniteScroll from 'react-infinite-scroller';
 import Image from '../components/Image';
 import useFetchImages from '../hooks/useRequest';
 
@@ -14,22 +13,14 @@ export default function Index() {
   if (error) return <div>Oops, something went wrong!</div>;
   if (!data) return <div>Loading...</div>;
 
-  const isLoadingMore = 0 < size && data && !data[size - 1];
-  const loadMore = () => {
-    if (!isLoadingMore) return setSize(size + 1);
-  };
-
   return (
     <main className={classNames.main}>
-      <InfiniteScroll
-        className={classNames.grid}
-        loadMore={loadMore}
-        hasMore={true}
-      >
-        {[].concat(...data).map((url: string, i: number) => (
-          <Image url={url} key={i} />
-        ))}
-      </InfiniteScroll>
+      <div className={classNames.grid}>
+        {data.map((images: string[]) => {
+          return images.map((url: string) => <Image url={url} key={url} />);
+        })}
+        <button onClick={() => setSize(size + 1)}>LOAD MORE</button>
+      </div>
     </main>
   );
 }
