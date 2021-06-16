516f1e673df3955c25c6b993bac30fb757a7f4fa
blooper05 <legend.of.blooper@gmail.com>
2021-06-17 00:44:24 +0900

    🔧 (chore): Use webpack4 explicitly for `windicss-webpack-plugin`

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const { default: WindiCSSWebpackPlugin } = require('windicss-webpack-plugin');
 
 module.exports = {
+  webpack5: false, // FIXME: refs. https://windicss.org/integrations/webpack.html
+
   webpack: (config) => {
     config.plugins.push(new WindiCSSWebpackPlugin());
     return config;
