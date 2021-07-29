61d8230930ec0d51e1407d9b2eb4f589831708aa
blooper05 <legend.of.blooper@gmail.com>
2021-07-30 00:12:10 +0900

    Revert "🔧 (chore): Use webpack4 explicitly for `windicss-webpack-plugin`"
    This reverts commit 516f1e673df3955c25c6b993bac30fb757a7f4fa.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 const { default: WindiCSSWebpackPlugin } = require('windicss-webpack-plugin');
 
 module.exports = {
-  webpack5: false, // FIXME: refs. https://windicss.org/integrations/webpack.html
-
   webpack: (config) => {
     config.plugins.push(new WindiCSSWebpackPlugin());
     return config;
