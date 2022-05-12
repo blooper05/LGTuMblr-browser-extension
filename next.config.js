fad3f830df4d32269f1e985c1ed31651c314edfd
blooper05 <legend.of.blooper@gmail.com>
2022-05-12 19:43:59 +0900

    ➖ (chore): Remove `Windi CSS` configurations

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-const WindiCSSWebpackPlugin = require('windicss-webpack-plugin');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.plugins.push(new WindiCSSWebpackPlugin());
-    return config;
-  },
-
   assetPrefix: 'assets',
 
   reactStrictMode: true,
