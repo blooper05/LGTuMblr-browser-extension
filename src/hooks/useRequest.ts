9b64825004edcc285766b341a74491b2c190c702
blooper05 <legend.of.blooper@gmail.com>
2022-05-16 15:22:47 +0900

    ♻️ (refactor): Use template literals to build the key in `getKey` function

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -5,8 +5,7 @@ const BASE_URL = 'https://lgtumblr-api.herokuapp.com/images';
 const getKey = (pageIndex: number, previousPageData: string[]) => {
   if (previousPageData && !previousPageData.length) return null;
 
-  const params = `?identifier=${pageIndex}`;
-  return BASE_URL + params;
+  return `${BASE_URL}?identifier=${pageIndex}`;
 };
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
